2f19f7091574fc7fbdb3aa2ed8df73cd74d67f49
Apoorva M K <42450012+apoorva-mk@users.noreply.github.com>
2020-09-05 22:31:42 +0530

    Create modular_exponentiation.cpp

---
+++ modular_exponentiation.cpp
@@ -0,0 +1,11 @@
+long long int modularExponentiation(long long int base, long long int exp, long long int mod){
+  long long ans = 1;
+  while(exp){
+    if(exp % 2){
+      ans = (ans * base) % mod;
+    }
+    base = (base * base) % mod;
+    exp = exp / 2;
+  }
+  return ans % mod;
+}
