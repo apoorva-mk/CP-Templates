6872e94bfce69ee69a0215162c48ee88a738d426
Apoorva M K <42450012+apoorva-mk@users.noreply.github.com>
2020-09-11 13:13:49 +0530

    Bellman ford - shortest path algo

---
+++ bellman_ford.cpp
@@ -0,0 +1,14 @@
+// Finding minimum distance of all vertices from a source vertex with negative weights too. 
+// Complexity of o(VE)
+// Can work for directed or undirected graphs
+// Edges represented as a vector with edges[0] = starting vertex, edges[1] = ending vertex, edes[2] = weight of edge
+vector<int> bellmanFord (vector <vector <int>> edges,int v, int e, int source) {
+    vector <int> distance (v, INT_MAX);
+    distance[source] = 0;
+    for (int i = 0; i < v; i++){
+        for (int j = 0; j < edges.size(); j++) {
+            if (distance[edges[i][0]] != INT_MAX && distance[edges[i][1]] < edges[i][2] + distance[edges[i][0]])
+                distance[edges[i][1]] = edges[i][2] + distance[edges[i][0]];
+        }
+    }
+}
